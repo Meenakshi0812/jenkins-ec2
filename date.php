aa21451cb304fdb3a249176c78939378206d7922
Meenakshi0812 <68263695+Meenakshi0812@users.noreply.github.com>
2023-05-08 21:51:18 +0530

    Create date.php

---
+++ date.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>Current Date and Time</title>
+  </head>
+  <body>
+    <h1>Current Date and Time</h1>
+    <p><?php echo "Today is " . date("Y/m/d") . "<br>"; ?></p>
+    <p><?php echo "The time is " . date("h:i:sa"); ?></p>
+  </body>
+</html>
